e4782912111a56a526c1085398f403c315b32e65
dtran39 <trananhduc@gatech.edu>
2017-02-26 16:16:29 -0500

    Initialize google maps, fix noId warning

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -8,8 +8,8 @@ export default class Header extends React.Component {
     return (
       <header className='Header'>
         <b>Header</b> &nbsp;
-        <NavLink activeClassName="active" exact to="/">Home</NavLink>
-        <NavLink activeClassName="active" to="/about">About Page</NavLink>
+        <NavLink activeClassName="active" exact to="/">List</NavLink>
+        <NavLink activeClassName="active" to="/about">GoogleMaps</NavLink>
         <NavLink to="/bad-url">Not Found Page</NavLink>
         <LoginButtons align='left' />
       </header>
--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
 var data = require('./example.json');
+var tableHeaders = '';
 var Home = React.createClass({
 	getInitialState: function(){
 		return {
 			peopleData: data,
-			peopleAddress: data.map(function(aPerson){
-				return (<tr>
+			peopleAddress: data.map(function(aPerson, anIndex){
+				return (<tr key={anIndex}>
 					<td>{aPerson.first_name} {aPerson.last_name}</td>
 					<td>{aPerson.address}, {aPerson.city}, {aPerson.state}, {aPerson.zip}</td>
 					<td>{aPerson.company_name}</td>
@@ -18,7 +19,10 @@ var Home = React.createClass({
 	},
 	render: function(){
 		return (
-			<table>{this.state.peopleAddress}</table>
+			<table>
+				<thead>{tableHeaders}</thead>
+				<tbody>{this.state.peopleAddress}</tbody>
+			</table>
 		);
 	}
 });
